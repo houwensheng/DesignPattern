b089009a99e62506fcdca5dfac4ec343e76e1b0c
houwensheng <houwsh@outlook.com>
2018-05-26 19:08:59 +0800

    add adapter pattern

---
+++ src/adapter/classs/Adaptee.java
@@ -0,0 +1,12 @@
+package adapter.classs;
+
+/**
+ * 源角色Adaptee是一个具体类，它有一个sampleOperation1()方法，但是没有sampleOperation2()方法。
+ * @author HR
+ *
+ */
+public class Adaptee {
+    
+    public void sampleOperation1(){}
+
+}

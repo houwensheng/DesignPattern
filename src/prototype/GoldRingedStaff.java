3b6c6927353781c391f769de24080fa15f8b2d2e
houwensheng <houwsh@outlook.com>
2018-05-28 23:03:07 +0800

    prototype
    prototype

---
+++ src/prototype/GoldRingedStaff.java
@@ -0,0 +1,27 @@
+package prototype;
+
+import java.io.Serializable;
+
+public class GoldRingedStaff implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private float height = 100.0f;
+    private float diameter = 10.0f;
+    /**
+     * 增长行为，每次调用长度和半径增加一倍
+     */
+    public void grow(){
+        this.diameter *= 2;
+        this.height *= 2;
+    }
+    /**
+     * 缩小行为，每次调用长度和半径减少一半
+     */
+    public void shrink(){
+        this.diameter /= 2;
+        this.height /= 2;
+    }
+}
